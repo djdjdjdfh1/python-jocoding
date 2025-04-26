7ac71e849aa5b22cf55584c3cdda2bf1506913c2
Park Chan <qkrcks8028@gmail.com>
2025-04-26 10:05:33 +0900

    python 문자열

---
+++ chapter2/2-2.py
@@ -0,0 +1,5 @@
+a = "%0.1f" % 3.412122
+print(a)
+b = '문자'
+format = f'format {b}입니다.'
+print(format)
--- chapter2/test.py
+++ chapter2/test.py
@@ -1,5 +1,9 @@
+text = "Hello world"
 a = 1
 b = 2.1
-print("Hello world!")
-print(type(a))
-print(type(b))
+# print(text * 10)
+print(text[:])
+print("I eat %s apples." % text)
+print("I eat %d apples." % 3)
+print("I eat %s apples." % "five")
+print('I ate %s apples. so I was sick for %s days.' % (4, a))
